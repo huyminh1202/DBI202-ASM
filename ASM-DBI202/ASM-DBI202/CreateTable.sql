4bd065fbb225f38e1f7f7598805b0f63eab80cd3
huyminh1202 <107800104+huyminh1202@users.noreply.github.com>
2022-06-20 00:26:44 +0700

    Update CreateTable.sql

--- ASM-DBI202/ASM-DBI202/CreateTable.sql
+++ ASM-DBI202/ASM-DBI202/CreateTable.sql
@@ -23,10 +23,33 @@ create table Student (
 	PhoneNumber varchar(10)
  )
 
+ Create table Assessment (
+	SubjectID int foreign key references Subject(SubjectID),
+	AssessmentID int primary key,
+	Category varchar(30) NOT NULL,
+	[Type] varchar(10) NOT NULL,
+	part int NOT NULL,
+	[Weight] varchar(10) NOT NULL,
+	[Completion Criteria] varchar(10) NOT NULL,
+	Duration varchar (10) NOT NULL,
+	LQ varchar(20),
+	[Question Type] varchar(200) NOT NULL,
+	[No Question] varchar (10),
+	[Knowledge and Skill] varchar(200),
+	[Note] varchar (200)
+	)
+
+	Create table Semester (
+		SemesterID int primary key,
+		SemesterName varchar(20) NOT NULL
+	)
+
 Create table [Group] (
 	GroupID int primary key,
 	GroupName varchar(30) NOT NULL,
 	LectureID int foreign key references Lecture(LectureID),
 	SubjectID int foreign key references Subject(SubjectID),
-	AssessmentID int
+	AssessmentID int foreign key references Assessment(AssessmentID),
+	SemesterID int foreign key references Semester(SemesterID)
 )
+
