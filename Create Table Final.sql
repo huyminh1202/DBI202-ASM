a25a9ff4909c593ac38ff473519719f5b5fc4946
huyminh1202 <107800104+huyminh1202@users.noreply.github.com>
2022-07-18 02:53:42 +0700

    CreateTable
    New Update

--- Create Table Final.sql
+++ Create Table Final.sql
@@ -1,62 +1,71 @@
 
 CREATE TABLE Student (
 	StudentID varchar(10) PRIMARY KEY,
-	StudentName nvarchar(30) NOT NULL,
-	DoB date NOT NULL,
-	Address nvarchar(30) NOT NULL,
-	PhoneNumber varchar (10)
+	StudentName nvarchar(50) NOT NULL,
+	Gender BIT,
+	DoB date,
+	Address nvarchar(30)
  )
 
  CREATE TABLE Subject (
-	SubjectID int PRIMARY KEY,
-	SubjectCode varchar(10) NOT NULL,
-	SubjectName varchar(30) NOT NULL,
-	StartDate date, 
-	EndDate date 
+	SubjectID varchar(10) PRIMARY KEY,
+	SubjectName nvarchar(30) NOT NULL,
  )
 
+ CREATE TABLE Subject_Semester (
+	SSID varchar(20) PRIMARY KEY,
+	SubjectID varchar(10) FOREIGN KEY REFERENCES [Subject](SubjectID),
+	Semester varchar(20),
+	StartDate DATE,
+	EndDate DATE
+)
 
- CREATE TABLE Lecture (
+
+ CREATE TABLE Assessment(
+	AssessmentID varchar(20) PRIMARY KEY,
+	SSID varchar(20)  FOREIGN KEY REFERENCES [Subject_Semester](SSID),
+	[Category] nvarchar(150),
+	[Weight] float,
+)
+
+CREATE TABLE Lecture (
 	LectureID int PRIMARY KEY,
 	LectureName nvarchar(30) NOT NULL,
 	Email varchar(30) NOT NULL,
 	PhoneNumber varchar(10) NOT NULL
  )
 
- CREATE TABLE Assessment (
-	Category varchar(20) PRIMARY KEY NOT NULL,
-	SubjectID int FOREIGN KEY REFERENCES Subject(SubjectID),
-	[Type] nvarchar(50) not null CHECK ([Type] in ('quiz','on-going','pratical exam','final exam')),
-	[Weight] float NOT NULL,
-	[Completion Criteria] varchar(10) NOT NULL,
-	Duration varchar (30) NOT NULL,
-	LO varchar(20),
-	[Question Type] varchar(200) NOT NULL,
-	[Number of Question] int ,
-	[Knowledge and Skill] varchar(200),
-	[Grading Guide] varchar (150),
-	[Note] varchar (200)
-	)
-
 
 CREATE TABLE [Group] (
-	GroupID varchar(10) PRIMARY KEY NOT NULL,
-	LectureID int FOREIGN KEY REFERENCES Lecture(LectureID),
-	SubjectID int FOREIGN KEY REFERENCES Subject(SubjectID),
-	Semester varchar(30)  NOT NULL
+	GroupID int PRIMARY KEY,
+	[Group Name] varchar(20)
+	
 )
 
+CREATE TABLE [Group_Student] (
+	GroupID int FOREIGN KEY REFERENCES  [Group](GroupID),
+	StudentID varchar(10) FOREIGN KEY REFERENCES [Student] (StudentID)
+)
 
+CREATE TABLE [Group_Lecture] (
+	GroupID int FOREIGN KEY REFERENCES  [Group](GroupID),
+	LectureID int FOREIGN KEY REFERENCES [Lecture] (LectureID)
+)
 
-CREATE TABLE [Result of Semester] (
-	StudentID varchar(10) FOREIGN KEY REFERENCES Student(StudentID),
-	SubjectID int FOREIGN KEY REFERENCES Subject(SubjectID),
-	Category varchar(20) FOREIGN KEY REFERENCES Assessment(Category),
-	[Grade Item] varchar(20) NOT NULL,
-	[Weight] float NOT NULL,
-	[Value] float NOT NULL,
-	Comment varchar(20)
+CREATE TABLE [Group_Subject](
+	GroupID int FOREIGN KEY REFERENCES  [Group](GroupID),
+	SubjectID varchar(10) FOREIGN KEY REFERENCES  [Subject](SubjectID)
+)
+
+CREATE TABLE [Lecture_Subject] (
+	LectureID int FOREIGN KEY REFERENCES [Lecture] (LectureID),
+	SubjectID varchar(10) FOREIGN KEY REFERENCES [Subject](SubjectID)
+)
  
+CREATE TABLE [Result Of Semester] (
+	StudentID varchar(10) FOREIGN KEY REFERENCES [Student] (StudentID),
+	AssessmentID varchar(20) FOREIGN KEY REFERENCES Assessment (AssessmentID),
+	[Score] float
 )
 
 
